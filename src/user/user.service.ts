b9455d72c82ad17960f488d5f055ffe542906be2
Leonardo Lüders <luderser@hotmail.com>
2025-07-24 10:53:35 -0300

    Injected userRepository() in the user service.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+}
