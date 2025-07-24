41b1252ea2d134ecfc4e844c15ea6ada09d5a9f2
Leonardo Lüders <luderser@hotmail.com>
2025-07-24 09:09:14 -0300

    Added .env-EXAMPLE file and configured NestJS ConfigModule as global.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
