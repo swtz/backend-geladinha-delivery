8a193281dc24ce03923b9fdec44770c98136388d
Leonardo Lüders <luderser@hotmail.com>
2025-07-24 10:29:09 -0300

    Configured TypeOrmModule using environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,38 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     AuthModule,
+    UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    UserModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DB_TYPE === 'better-sqlite3') {
+          return {
+            type: process.env.DB_TYPE,
+            database: process.env.DB_DATABASE || './db.sqlite',
+            synchronize: true,
+            autoLoadEntities: true,
+          };
+        }
+
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT || '5432', 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
+    }),
   ],
 })
 export class AppModule {}
