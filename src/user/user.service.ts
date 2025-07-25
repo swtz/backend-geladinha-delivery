47580d09a1e691f7f77eac450ad28d2c9b2cf814
Leonardo Lüders <luderser@hotmail.com>
2025-07-25 14:05:02 -0300

    Started the create() in the user service.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -9,4 +10,16 @@ export class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
+
+  async create(dto: CreateUserDto) {
+    const exists = await this.userRepository.exists({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (exists) {
+      throw new ConflictException('Email já existe');
+    }
+  }
 }
