3f8ed27f7832af7514c9a49e5d820ca0dd44829d
Leonardo Lüders <luderser@hotmail.com>
2025-07-25 14:42:37 -0300

    Implemented validations using class-validator decorators in the create user DTO file.

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo nome não pode estar vazio' })
+  name: string;
+
+  @IsEmail({}, { message: 'E-mail inválido' })
+  email: string;
+
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo senha não pode estar vazio' })
+  password: string;
+}
