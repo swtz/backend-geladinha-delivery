7dc1b4c31687277ed17ec8215c25eaf893d4a9ec
Leonardo Lüders <luderser@hotmail.com>
2025-07-28 11:11:41 -0300

    Created LoginDto.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,1 +1,10 @@
-export class AuthDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail({}, { message: 'E-mail inválido' })
+  email: string;
+
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo senha não pode estar vazio' })
+  password: string;
+}
