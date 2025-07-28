6b89287c6daf121986a69f1ceaa28d3336e2e935
Leonardo Lüders <luderser@hotmail.com>
2025-07-28 11:05:06 -0300

    Injected the UserService and the HashingService in the AuthService.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
+  imports: [UserModule, CommonModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { HashingService } from 'src/common/hashing/hashing.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly hashingService: HashingService,
+  ) {}
+}
