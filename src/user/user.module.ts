726523241867ffd851d173b8274f1b32a14e416a
Leonardo Lüders <luderser@hotmail.com>
2025-07-28 10:27:39 -0300

    Injected the HashingService into the UserService.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), CommonModule],
   controllers: [UserController],
   providers: [UserService],
 })
