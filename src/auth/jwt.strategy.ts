2da517bbd9a81bbb4049b0053fd56ab670760442
Leonardo Lüders <luderser@hotmail.com>
2025-07-28 14:43:38 -0300

    Called super() from PassportStrategy to extract the JWT token from bearer authorization header.

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) {
+      throw new InternalServerErrorException(
+        'JWT_SECRET not found in .env file',
+      );
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
   validate(...args: any[]): unknown {}
 }
