93b59dd4344874cc8477a8779906ee52667b6367
Leonardo Lüders <luderser@hotmail.com>
2025-07-28 11:25:20 -0300

    Started login() in the auth service.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { HashingService } from 'src/common/hashing/hashing.service';
 import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,24 @@ export class AuthService {
     private readonly userService: UserService,
     private readonly hashingService: HashingService,
   ) {}
+
+  async login(dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email);
+    const error = new UnauthorizedException('Usuário ou senha inválidos');
+
+    if (!user) {
+      throw error;
+    }
+
+    const validPassword = await this.hashingService.compare(
+      dto.password,
+      user.password,
+    );
+
+    if (!validPassword) {
+      throw error;
+    }
+
+    return dto;
+  }
 }
