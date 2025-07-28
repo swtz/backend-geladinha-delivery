22b9d1210a5affcfbcc18a3d52ebad03533c2c64
Leonardo Lüders <luderser@hotmail.com>
2025-07-28 14:34:34 -0300

    Started JwtStrategy class.

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  validate(...args: any[]): unknown {}
+}
