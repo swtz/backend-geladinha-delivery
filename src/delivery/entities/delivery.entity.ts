332ac1ad27ae722421935d855921a25edc36145f
Leonardo Lüders <luderser@hotmail.com>
2025-07-29 16:09:37 -0300

    Created the delivery entity.

--- src/delivery/entities/delivery.entity.ts
+++ src/delivery/entities/delivery.entity.ts
@@ -1,1 +1,27 @@
-export class DeliveryEntity {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class DeliveryEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  name: string;
+  @Column({ type: 'double' })
+  totalPurchase: number;
+  @Column({ type: 'double' })
+  deliveryTax: number;
+  @Column()
+  paymentMethod: string;
+  @Column({ default: false })
+  paid: boolean;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
