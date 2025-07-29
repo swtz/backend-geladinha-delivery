aca5a8b900e9179a37f227bbcb04fca39be87a37
Leonardo Lüders <luderser@hotmail.com>
2025-07-29 16:16:13 -0300

    Injected the deliveryRepository in the delivery service.

--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DeliveryService } from './delivery.service';
 import { DeliveryController } from './delivery.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeliveryEntity } from './entities/delivery.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DeliveryEntity])],
   controllers: [DeliveryController],
   providers: [DeliveryService],
 })
--- src/delivery/delivery.service.ts
+++ src/delivery/delivery.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { DeliveryEntity } from './entities/delivery.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class DeliveryService {}
+export class DeliveryService {
+  constructor(
+    @InjectRepository(DeliveryEntity)
+    private readonly deliveryRepository: Repository<DeliveryEntity>,
+  ) {}
+}
