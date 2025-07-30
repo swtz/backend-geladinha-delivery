590af0915f74faeef3b5d619ac606bf201fc4f18
Leonardo Lüders <luderser@hotmail.com>
2025-07-30 10:03:47 -0300

    Created the create delivery DTO.

--- src/delivery/dto/create-delivery.dto.ts
+++ src/delivery/dto/create-delivery.dto.ts
@@ -1,1 +1,22 @@
-export class CreateDeliveryDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateDeliveryDto {
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo nome não pode estar vazio' })
+  name: string;
+
+  @IsNumber(
+    {},
+    { message: 'Campo valor total da compra precisa ser um número' },
+  )
+  @IsNotEmpty({ message: 'Campo valor total da compra não pode estar vazio' })
+  totalPurchase: number;
+
+  @IsNumber({}, { message: 'Campo valor da entrega precisa ser um número' })
+  @IsNotEmpty({ message: 'Campo valor da entrega não pode estar vazio' })
+  deliveryTax: number;
+
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo método de pagamento não pode estar vazio' })
+  paymentMethod: string;
+}
--- src/delivery/entities/delivery.entity.ts
+++ src/delivery/entities/delivery.entity.ts
@@ -10,18 +10,25 @@ import {
 export class DeliveryEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   name: string;
+
   @Column({ type: 'double' })
   totalPurchase: number;
+
   @Column({ type: 'double' })
   deliveryTax: number;
+
   @Column()
   paymentMethod: string;
+
   @Column({ default: false })
   paid: boolean;
+
   @CreateDateColumn()
   createdAt: Date;
+
   @UpdateDateColumn()
   updatedAt: Date;
 }
