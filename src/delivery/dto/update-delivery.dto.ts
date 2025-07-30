7687c184e42b912efc281085719aee79840bdf79
Leonardo Lüders <luderser@hotmail.com>
2025-07-30 10:13:23 -0300

    Created the update delivery DTO.

--- src/delivery/dto/update-delivery.dto.ts
+++ src/delivery/dto/update-delivery.dto.ts
@@ -1,1 +1,9 @@
-export class UpdateDeliveryDto {}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateDeliveryDto } from './create-delivery.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+
+export class UpdateDeliveryDto extends PartialType(CreateDeliveryDto) {
+  @IsOptional()
+  @IsBoolean({ message: 'Campo pago precisa ser verdadeiro/falso' })
+  paid?: boolean;
+}
