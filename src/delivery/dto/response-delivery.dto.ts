1b0618fa0eb83d2ccc4ce2edc8ba7ebd4017c4d6
Leonardo Lüders <luderser@hotmail.com>
2025-07-30 10:40:24 -0300

    Created the response delivery DTO.

---
+++ src/delivery/dto/response-delivery.dto.ts
@@ -0,0 +1,23 @@
+import { DeliveryEntity } from '../entities/delivery.entity';
+
+export class ResponseDeliveryDto {
+  readonly id: string;
+  readonly name: string;
+  readonly paymentMethod: string;
+  readonly totalPurchase: number;
+  readonly deliveryTax: number;
+  readonly paid: boolean;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+
+  constructor(delivery: DeliveryEntity) {
+    this.id = delivery.id;
+    this.name = delivery.name;
+    this.paymentMethod = delivery.paymentMethod;
+    this.totalPurchase = delivery.totalPurchase;
+    this.deliveryTax = delivery.deliveryTax;
+    this.paid = delivery.paid;
+    this.createdAt = delivery.createdAt;
+    this.updatedAt = delivery.updatedAt;
+  }
+}
