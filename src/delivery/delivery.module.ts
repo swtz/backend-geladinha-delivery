950880a22ca4657087e1bec8a500b162dd5c760d
Leonardo Lüders <luderser@hotmail.com>
2025-08-01 10:02:38 -0300

    Started the create() and injected the userService in the delivery service.

--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -3,9 +3,10 @@ import { DeliveryService } from './delivery.service';
 import { DeliveryController } from './delivery.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeliveryEntity } from './entities/delivery.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DeliveryEntity])],
+  imports: [TypeOrmModule.forFeature([DeliveryEntity]), UserModule],
   controllers: [DeliveryController],
   providers: [DeliveryService],
 })
--- src/delivery/delivery.service.ts
+++ src/delivery/delivery.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { DeliveryEntity } from './entities/delivery.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateDeliveryDto } from './dto/create-delivery.dto';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class DeliveryService {
   constructor(
     @InjectRepository(DeliveryEntity)
     private readonly deliveryRepository: Repository<DeliveryEntity>,
+    private readonly userService: UserService,
   ) {}
+
+  async create(dto: CreateDeliveryDto, user: UserEntity) {
+    const operator = await this.userService.findOneByOrFail({ id: user.id });
+  }
 }
