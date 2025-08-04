3eddfccc0d59cb7f51af4e7b87924af573b23aeb
Leonardo Lüders <luderser@hotmail.com>
2025-08-04 15:18:27 -0300

    Injected the voucher repository in the voucher service.

--- src/voucher/voucher.module.ts
+++ src/voucher/voucher.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VoucherService } from './voucher.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VoucherEntity } from './entities/voucher.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([VoucherEntity])],
   providers: [VoucherService],
   exports: [VoucherService],
 })
--- src/voucher/voucher.service.ts
+++ src/voucher/voucher.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { VoucherEntity } from './entities/voucher.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class VoucherService {}
+export class VoucherService {
+  constructor(
+    @InjectRepository(VoucherEntity)
+    private readonly voucherRepository: Repository<VoucherEntity>,
+  ) {}
+}
