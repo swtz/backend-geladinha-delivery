b4e19714497c9698bfcd34ccf348ace0e4349b18
Leonardo Lüders <luderser@hotmail.com>
2025-08-04 13:46:07 -0300

    Started the delivery man entity.

--- src/delivery-man/entities/delivery-man.entity.ts
+++ src/delivery-man/entities/delivery-man.entity.ts
@@ -1,1 +1,43 @@
-export class DeliveryManEntity {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class DeliveryManEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  phone: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  forceLogout: boolean;
+
+  @Column()
+  motorcycle: string;
+
+  @Column('double', { nullable: true })
+  tip: number;
+
+  @Column('double')
+  daily: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
