8bbf1bdf83c54086b2685225f5f2052b876115ae
Leonardo Lüders <luderser@hotmail.com>
2025-08-04 15:43:53 -0300

    Injected delivery man repository and voucher service in the delivery service.

--- src/delivery-man/delivery-man.module.ts
+++ src/delivery-man/delivery-man.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DeliveryManService } from './delivery-man.service';
 import { DeliveryManController } from './delivery-man.controller';
+import { VoucherModule } from 'src/voucher/voucher.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeliveryManEntity } from './entities/delivery-man.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DeliveryManEntity]), VoucherModule],
   controllers: [DeliveryManController],
   providers: [DeliveryManService],
 })
--- src/delivery-man/delivery-man.service.ts
+++ src/delivery-man/delivery-man.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { VoucherService } from 'src/voucher/voucher.service';
+import { Repository } from 'typeorm';
+import { DeliveryManEntity } from './entities/delivery-man.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class DeliveryManService {}
+export class DeliveryManService {
+  constructor(
+    @InjectRepository(DeliveryManEntity)
+    private readonly deliveryManRepository: Repository<DeliveryManEntity>,
+    private readonly voucherService: VoucherService,
+  ) {}
+}
