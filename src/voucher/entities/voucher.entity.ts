645bfd56b6da54e01622691183c4135dcbde5217
Leonardo Lüders <luderser@hotmail.com>
2025-08-04 15:12:46 -0300

    Created the voucher entity.

--- src/voucher/entities/voucher.entity.ts
+++ src/voucher/entities/voucher.entity.ts
@@ -1,1 +1,30 @@
-export class VoucherEntity {}
+import { DeliveryManEntity } from 'src/delivery-man/entities/delivery-man.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class VoucherEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('double')
+  amount: number;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => DeliveryManEntity)
+  deliveryMan: DeliveryManEntity;
+}
