4106fa001fab5befefe0ae14ebff5f98a9aa4c80
Leonardo Lüders <luderser@hotmail.com>
2025-08-07 12:03:01 -0300

    Updated AuthenticatedRequest.user type.

--- src/auth/types/authenticated-request.type.ts
+++ src/auth/types/authenticated-request.type.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
+import { DeliveryManEntity } from 'src/delivery-man/entities/delivery-man.entity';
 import { UserEntity } from 'src/user/entities/user.entity';
 
 export interface AuthenticatedRequest extends Request {
-  user: UserEntity;
+  user: UserEntity | DeliveryManEntity;
 }
