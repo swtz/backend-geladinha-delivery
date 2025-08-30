96a2a6dfb50e49c3ab8dc8541207fb4e03ae9e59
Leonardo Lüders <luderser@hotmail.com>
2025-08-30 11:52:01 -0300

    Created address entity.

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,1 +1,34 @@
-export class AddressEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 48 })
+  street: string;
+
+  @Column({ length: 16 })
+  number: string;
+
+  @Column({ length: 32 })
+  complement: string;
+
+  @Column({ length: 32 })
+  referencePoint: string;
+
+  @Column({ length: 32 })
+  neighborhood: string;
+
+  @Column({ length: 32, default: '88955-000' })
+  postalCode: string;
+
+  @Column({ length: 32, default: 'Balneário Gaivota' })
+  city: string;
+
+  @Column({ length: 2, default: 'SC' })
+  stateCode: string;
+
+  @Column({ length: 32 })
+  location: string;
+}
