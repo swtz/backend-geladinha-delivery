e7590551b0937087152abf6c1f8befc9eb54a8a9
Leonardo Lüders <luderser@hotmail.com>
2025-09-01 13:33:54 -0300

    Injected address service in the customer service using NestJS DI system.

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -3,9 +3,10 @@ import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './entities/customer.entity';
+import { AddressModule } from 'src/address/address.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer])],
+  imports: [TypeOrmModule.forFeature([Customer]), AddressModule],
   controllers: [CustomerController],
   providers: [CustomerService],
 })
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Customer } from './entities/customer.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AddressService } from 'src/address/address.service';
 
 @Injectable()
 export class CustomerService {
   constructor(
     @InjectRepository(Customer)
     private readonly customerRepository: Repository<Customer>,
+    private readonly addressService: AddressService,
   ) {}
 }
