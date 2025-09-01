7cd79a58fb56dbda5f30c4d70e357d8098426b84
Leonardo Lüders <luderser@hotmail.com>
2025-09-01 13:30:31 -0300

    Created AddressService.create() and save() that using the logger property to display errors stack in the terminal when this method fail.

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Address } from './entities/address.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateAddressDto } from './dto/create-address.dto';
 
 @Injectable()
-export class AddressService {}
+export class AddressService {
+  private readonly logger = new Logger(AddressService.name);
+
+  constructor(
+    @InjectRepository(Address)
+    private readonly addressRepository: Repository<Address>,
+  ) {}
+
+  create(dto: CreateAddressDto) {
+    const newAddress = {
+      street: dto.street,
+      number: dto.number,
+      complement: dto.complement,
+      referencePoint: dto.referencePoint,
+      neighborhood: dto.neighborhood,
+      postalCode: dto.postalCode,
+      city: dto.city,
+      stateCode: dto.stateCode,
+      location: dto.location,
+      isDefault: dto.isDefault,
+    };
+
+    return this.save(newAddress);
+  }
+
+  async save(address: Partial<Address>) {
+    const created = await this.addressRepository
+      .save(address)
+      .catch((err: unknown) => {
+        if (err instanceof Error) {
+          this.logger.error('Erro ao criar endereço', err.stack);
+        }
+
+        throw new BadRequestException('Erro ao criar endereço');
+      });
+
+    return created;
+  }
+}
