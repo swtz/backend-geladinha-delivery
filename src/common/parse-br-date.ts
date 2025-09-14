8a0c5ed77926ce2057e5b34c0b861054d779b19f
Leonardo Lüders <luderser@hotmail.com>
2025-09-14 13:58:21 -0300

    Updated ParseBrDatePipe using parseBrDate().

--- src/common/parse-br-date.ts
+++ src/common/parse-br-date.ts
@@ -3,8 +3,9 @@ import { parse } from 'date-fns';
 
 export function parseBrDate(date: string, hour: string) {
   const newDate = parse(`${date} ${hour}`, 'dd/MM/yyyy HH:mm', new Date());
+  const isInvalidYear = newDate.getFullYear().toString(10).length < 4;
 
-  if (!newDate.valueOf()) {
+  if (!newDate.valueOf() || isInvalidYear) {
     throw new BadRequestException('Data inválida');
   }
 
