5f1eae472c53e1b6e8c5f74bffdbde87c3ae5660
Leonardo Lüders <luderser@hotmail.com>
2025-09-14 10:35:59 -0300

    Created the payout entity and its relationships.

--- src/payout/entities/payout.entity.ts
+++ src/payout/entities/payout.entity.ts
@@ -1,1 +1,44 @@
-export class Payout {}
+import { DeliveryMan } from 'src/user/entities/user.entity';
+import { Voucher } from 'src/voucher/entities/voucher.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Payout {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('double')
+  totalDeliveries: number;
+
+  @Column('double')
+  motoboyDaily: number;
+
+  @Column('double')
+  subtotal: number;
+
+  @Column('double')
+  totalSpending: number;
+
+  @Column('double')
+  total: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => DeliveryMan, { onDelete: 'CASCADE' })
+  motoboy: DeliveryMan;
+
+  @OneToMany(() => Voucher, voucher => voucher.payout, { nullable: true })
+  vouchers: Voucher[];
+}
--- src/payout/payout.module.ts
+++ src/payout/payout.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PayoutService } from './payout.service';
 import { PayoutController } from './payout.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payout } from './entities/payout.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Payout])],
   controllers: [PayoutController],
   providers: [PayoutService],
 })
