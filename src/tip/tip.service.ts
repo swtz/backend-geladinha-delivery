c0eebf76aa5b4c4b1d2b32e004d1ce5cd2132e26
Leonardo Lüders <luderser@hotmail.com>
2025-09-25 09:54:34 -0300

    Created TipService.create() and save().

--- src/tip/tip.service.ts
+++ src/tip/tip.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Tip } from './entities/tip.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TipService {}
+export class TipService {
+  private readonly logger = new Logger(TipService.name);
+
+  constructor(
+    @InjectRepository(Tip)
+    private readonly tipRepository: Repository<Tip>,
+  ) {}
+
+  create(amount: number) {
+    return this.save({ amount });
+  }
+
+  async save(tip: Partial<Tip>) {
+    const created = await this.tipRepository.save(tip).catch((err: unknown) => {
+      if (err instanceof Error) {
+        this.logger.error('Erro ao criar gorjeta', err.stack);
+      }
+
+      throw new BadRequestException('Erro ao criar gorjeta');
+    });
+
+    return created;
+  }
+}
