52f518efae2cd12bcd400174009d176eaf301dfb
Leonardo Lüders <luderser@hotmail.com>
2025-09-25 09:08:10 -0300

    Created the tip entity.

--- src/tip/entities/tip.entity.ts
+++ src/tip/entities/tip.entity.ts
@@ -1,1 +1,22 @@
-export class Tip {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Tip {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('double')
+  amount: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
--- src/tip/tip.module.ts
+++ src/tip/tip.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TipService } from './tip.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tip } from './entities/tip.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tip])],
   providers: [TipService],
 })
 export class TipModule {}
