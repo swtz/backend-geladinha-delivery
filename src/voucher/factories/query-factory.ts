76f10756c08346da3f954e22f36f4141fcd44cac
Leonardo Lüders <luderser@hotmail.com>
2025-10-06 09:11:58 -0300

    Created AbstractMethod and FindAllParams type for factoryMethod().

--- src/voucher/factories/query-factory.ts
+++ src/voucher/factories/query-factory.ts
@@ -1,5 +1,5 @@
 import { User } from 'src/user/entities/user.entity';
-import { FindOperator } from 'typeorm';
+import { Between, FindOperator } from 'typeorm';
 
 export interface Query {
   createdAt?: FindOperator<Date>;
@@ -9,4 +9,23 @@ export class VoucherFindAllQuery implements Query {
   user?: Partial<User>;
   createdBy?: Partial<User>;
   createdAt?: FindOperator<Date>;
+}
+
+type DateParams = {
+  fromDate?: Date;
+  toDate?: Date;
+};
+
+export type FindAllParams = {
+  user?: User;
+} & DateParams;
+
+abstract class AbstractMethod {
+  getDatePeriod(fromDate?: Date, toDate?: Date) {
+    if (fromDate !== undefined && toDate !== undefined) {
+      return Between(fromDate, toDate);
+    }
+  }
+
+  abstract factoryMethod(params: FindAllParams): Query;
 }
