f0212c0217fe9e9bec61c4f99182cc204c3f3a5a
Leonardo Lüders <luderser@hotmail.com>
2025-10-07 20:03:29 -0300

    Added isPaid property to the Query interface in the delivery query factory.

--- src/delivery/factories/query-factory..ts
+++ src/delivery/factories/query-factory..ts
@@ -5,6 +5,7 @@ import { PaymentMethod } from '../entities/payment-method.entity';
 import { PaymentMethod as PaymentMethodEnum } from '../enums/payment-methods.enum';
 
 interface Query {
+  isPaid?: boolean;
   createdAt?: FindOperator<Date>;
 }
 
@@ -12,19 +13,21 @@ class DeliveryFindAllQuery implements Query {
   customer?: Partial<Customer>;
   motoboy?: Partial<DeliveryMan>;
   operator?: Partial<User>;
-  isPaid?: boolean;
   paymentMethod?: Partial<PaymentMethod>;
+  isPaid?: boolean;
   createdAt?: FindOperator<Date>;
 }
 
 class DeliveryTaxQuery implements Query {
   motoboy?: Partial<User>;
+  isPaid?: boolean;
   createdAt?: FindOperator<Date>;
 }
 
 class TotalPurchaseQuery implements Query {
   operator?: Partial<User>;
   paymentMethod?: Partial<PaymentMethod>;
+  isPaid?: boolean;
   createdAt?: FindOperator<Date>;
 }
 
@@ -43,6 +46,7 @@ export type FindAllParams = {
 
 export type SumDeliveryTaxParams = {
   user?: User;
+  isPaid?: boolean;
 } & DateParams;
 
 export type SumTotalPurchaseParams = {
@@ -83,11 +87,17 @@ export class DeliveryFindAllFactory extends AbstractFactory {
 }
 
 export class DeliveryTaxFactory extends AbstractFactory {
-  factoryMethod({ user, fromDate, toDate }: SumDeliveryTaxParams): Query {
+  factoryMethod({
+    user,
+    fromDate,
+    toDate,
+    isPaid,
+  }: SumDeliveryTaxParams): Query {
     const queryObject = new DeliveryTaxQuery();
 
     queryObject.createdAt = this.checkDateValue(fromDate, toDate);
     queryObject.motoboy = { id: user?.id };
+    queryObject.isPaid = isPaid;
 
     return queryObject;
   }
@@ -99,12 +109,14 @@ export class TotalPurchaseFactory extends AbstractFactory {
     fromDate,
     toDate,
     paymentMethod,
+    isPaid,
   }: SumTotalPurchaseParams): Query {
     const queryObject = new TotalPurchaseQuery();
 
     queryObject.createdAt = this.checkDateValue(fromDate, toDate);
     queryObject.operator = { id: user?.id };
     queryObject.paymentMethod = { name: paymentMethod };
+    queryObject.isPaid = isPaid;
 
     return queryObject;
   }
