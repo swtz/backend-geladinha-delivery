95fda7b97af9f66c10277ec9d54d8eed30da4ab2
Leonardo Lüders <luderser@hotmail.com>
2025-10-07 17:44:04 -0300

    Started Settlement.preview().

--- src/settlement/settlement.service.ts
+++ src/settlement/settlement.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Settlement } from './entities/settlement.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeliveryService } from 'src/delivery/delivery.service';
 import { UserService } from 'src/user/user.service';
 import { VoucherService } from 'src/voucher/voucher.service';
+import { User } from 'src/user/entities/user.entity';
+import { parseBrDate } from 'src/common/parse-br-date';
+import {
+  CURRENT_SHORT_DATE,
+  END_TIME,
+  START_TIME,
+} from 'src/common/operation-time';
 
 @Injectable()
 export class SettlementService {
@@ -15,4 +22,28 @@ export class SettlementService {
     private readonly voucherService: VoucherService,
     private readonly userService: UserService,
   ) {}
+
+  async preview(fromDate: Date, toDate: Date, userData: Partial<User>) {
+    if (!userData) {
+      throw new BadRequestException('Informe o nome do operador de caixa');
+    }
+
+    const currentFromDate = parseBrDate(CURRENT_SHORT_DATE, START_TIME);
+    const currentToDate = parseBrDate(CURRENT_SHORT_DATE, END_TIME);
+
+    const initDate = fromDate || currentFromDate;
+    const endDate = toDate || currentToDate;
+
+    const operator = await this.userService.findOneByOrFail(userData);
+    const vouchers = await this.voucherService.findAllOwned({
+      user: operator,
+      fromDate: initDate,
+      toDate: endDate,
+    });
+    const deliveries = await this.deliveryService.findAll({
+      fromDate: initDate,
+      toDate: endDate,
+      operatorName: userData.name,
+    });
+  }
 }
