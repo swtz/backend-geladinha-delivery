d084991d8ee57ccb6b581868ef98ee8cd177ed84
Leonardo Lüders <luderser@hotmail.com>
2025-10-07 17:28:44 -0300

    Injected dependencies in the settlement service using NestJS DI.

--- src/settlement/settlement.module.ts
+++ src/settlement/settlement.module.ts
@@ -3,9 +3,17 @@ import { SettlementService } from './settlement.service';
 import { SettlementController } from './settlement.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Settlement } from './entities/settlement.entity';
+import { DeliveryModule } from 'src/delivery/delivery.module';
+import { VoucherModule } from 'src/voucher/voucher.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Settlement])],
+  imports: [
+    TypeOrmModule.forFeature([Settlement]),
+    DeliveryModule,
+    VoucherModule,
+    UserModule,
+  ],
   controllers: [SettlementController],
   providers: [SettlementService],
 })
--- src/settlement/settlement.service.ts
+++ src/settlement/settlement.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Settlement } from './entities/settlement.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeliveryService } from 'src/delivery/delivery.service';
+import { UserService } from 'src/user/user.service';
+import { VoucherService } from 'src/voucher/voucher.service';
 
 @Injectable()
-export class SettlementService {}
+export class SettlementService {
+  constructor(
+    @InjectRepository(Settlement)
+    private readonly settlementRepository: Repository<Settlement>,
+    private readonly deliveryService: DeliveryService,
+    private readonly voucherService: VoucherService,
+    private readonly userService: UserService,
+  ) {}
+}
