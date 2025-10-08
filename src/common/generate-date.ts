805845e4dda85d9b5d5c47070a42ebf012c60bc1
Leonardo Lüders <luderser@hotmail.com>
2025-10-08 13:19:53 -0300

    Created auxiliar constant into generate date methods to avoid use date object reference.

--- src/common/generate-date.ts
+++ src/common/generate-date.ts
@@ -1,9 +1,11 @@
 export function generateYesterdayDate(date: Date = new Date()) {
-  const yesterday = date.setDate(date.getDate() - 1);
+  const auxDate = new Date(date);
+  const yesterday = auxDate.setDate(date.getDate() - 1);
   return new Date(yesterday);
 }
 
 export function generateTomorrowDate(date: Date = new Date()) {
-  const tomorrow = date.setDate(date.getDate() + 1);
+  const auxDate = new Date(date);
+  const tomorrow = auxDate.setDate(date.getDate() + 1);
   return new Date(tomorrow);
 }
