b97b9f274a529b837e07c319d125a171f8db9116
Leonardo Lüders <luderser@hotmail.com>
2025-10-14 12:05:24 -0300

    Enabled cors based in an origin whitelist.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { parseCorsWhitelist } from './common/utils/parse-cors-whitelist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,23 @@ async function bootstrap() {
       crossOriginResourcePolicy: { policy: 'cross-origin' },
     }),
   );
+
+  const corsWhiteList = parseCorsWhitelist(process.env.CORS_WHITELIST ?? '');
+
+  app.enableCors({
+    origin: (
+      origin: string | undefined,
+      callback: (...args: any[]) => void,
+    ) => {
+      if (!origin || corsWhiteList.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'), false);
+      // É possível mudar a abordagem acima, ou seja, não levantar uma exceção
+      // quando a Origin é desconhecida pelo servidor
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
