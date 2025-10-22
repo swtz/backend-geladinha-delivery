b9fac49e5c48a449659feb774c36829cb14feb30
Leonardo Lüders <luderser@hotmail.com>
2025-10-22 20:52:45 -0300

    Create CreateWorkTimeDto.

---
+++ src/place/dto/work-time/create-work-time.dto.ts
@@ -0,0 +1,16 @@
+import { IsBoolean, IsEnum, IsNumber } from 'class-validator';
+import { Shift } from 'src/common/enums/work-shifts.enum';
+
+export class CreateWorkTimeDto {
+  @IsEnum(Shift, { message: 'Turno inválido' })
+  shift: string;
+
+  @IsNumber({}, { message: 'Número inválido' })
+  initHour: number;
+
+  @IsNumber({}, { message: 'Número inválido' })
+  endHour: number;
+
+  @IsBoolean({ message: 'O campo só permite o formato verdadeiro/falso' })
+  isDefault: boolean;
+}
