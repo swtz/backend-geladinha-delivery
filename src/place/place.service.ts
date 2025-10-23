5d2b44a0a8a6dbeaef703410a296784417fb87a8
Leonardo Lüders <luderser@hotmail.com>
2025-10-23 15:23:43 -0300

    Injected the place repository into the place service.

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Place } from './entities/place.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class PlaceService {}
+export class PlaceService {
+  constructor(
+    @InjectRepository(Place)
+    private readonly placeRepository: Repository<Place>,
+  ) {}
+}
