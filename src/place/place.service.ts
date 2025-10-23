483869f99438a783cf3103da2eb4ce053850b80c
Leonardo Lüders <luderser@hotmail.com>
2025-10-23 15:26:20 -0300

    Created PlaceService.save().

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -1,12 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Place } from './entities/place.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { generateBadRequestException } from 'src/common/generate-exception';
 
 @Injectable()
 export class PlaceService {
+  private readonly logger = new Logger(PlaceService.name);
+
   constructor(
     @InjectRepository(Place)
     private readonly placeRepository: Repository<Place>,
   ) {}
+
+  async save(placeData: Partial<Place>) {
+    const http400 = generateBadRequestException(
+      'Erro ao salvar o estabelecimento',
+    );
+    const created = await this.placeRepository
+      .save(placeData)
+      .catch((err: unknown) => {
+        if (err instanceof Error) {
+          this.logger.error(http400.message, err.stack);
+        }
+
+        throw http400;
+      });
+
+    return created;
+  }
 }
