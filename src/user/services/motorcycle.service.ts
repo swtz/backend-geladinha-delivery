33309c9b35a4686f97b9662b5054cf8b6b39f5cb
Leonardo Lüders <luderser@hotmail.com>
2026-02-18 17:04:36 -0300

    Created MotorcycleService.save().

--- src/user/services/motorcycle.service.ts
+++ src/user/services/motorcycle.service.ts
@@ -1,10 +1,30 @@
 import { Repository } from 'typeorm';
 import { Motorcycle } from '../entities/motorcycle.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Logger } from '@nestjs/common';
+import { generateBadRequestException } from 'src/common/generate-exception';
 
 export class MotorcycleService {
+  private readonly logger = new Logger(MotorcycleService.name);
+
   constructor(
     @InjectRepository(Motorcycle)
     private readonly motorcycleRepository: Repository<Motorcycle>,
   ) {}
+
+  async save(motorcycle: Partial<Motorcycle>) {
+    const http400 = generateBadRequestException('Erro ao salvar a moto');
+
+    const created = this.motorcycleRepository
+      .save(motorcycle)
+      .catch((err: unknown) => {
+        if (err instanceof Error) {
+          this.logger.error(http400.message, err.stack);
+        }
+
+        throw http400;
+      });
+
+    return created;
+  }
 }
