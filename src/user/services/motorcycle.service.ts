b10c61e8d53aa6879de4ede50fe43c2cd8f36cbd
Leonardo Lüders <luderser@hotmail.com>
2026-02-18 17:32:03 -0300

    Injected the user service into the motorcycle service and initialized create().

--- src/user/services/motorcycle.service.ts
+++ src/user/services/motorcycle.service.ts
@@ -3,6 +3,8 @@ import { Motorcycle } from '../entities/motorcycle.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Logger } from '@nestjs/common';
 import { generateBadRequestException } from 'src/common/generate-exception';
+import { CreateMotorcycleDto } from '../dtos/motorcycle/create-motorcycle.dto';
+import { UserService } from '../user.service';
 
 export class MotorcycleService {
   private readonly logger = new Logger(MotorcycleService.name);
@@ -10,7 +12,25 @@ export class MotorcycleService {
   constructor(
     @InjectRepository(Motorcycle)
     private readonly motorcycleRepository: Repository<Motorcycle>,
+    private readonly userService: UserService,
   ) {}
+
+  async create(dto: CreateMotorcycleDto) {
+    const owner = await this.userService.findOneByOrFail({ id: dto.owner });
+    const driver = await this.userService.findOneMotoboyByOrFail({
+      id: dto.driver,
+    });
+
+    // dto.isActive aqui é necessário?
+
+    // verificar se o ano é valido
+    const year = dto.year;
+
+    // verificar se licensePlate é valida
+    const licensePlate = dto.licensePlate;
+
+    return this.save({ ...dto, year, licensePlate, owner, driver });
+  }
 
   async save(motorcycle: Partial<Motorcycle>) {
     const http400 = generateBadRequestException('Erro ao salvar a moto');
