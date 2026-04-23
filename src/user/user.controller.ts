8a2dc4636492581a97b8ca511ed81c728776e25b
Leonardo Lüders <luderser@hotmail.com>
2026-04-23 14:34:12 -0300

    Refactored UserController.create() injecting the MotorcycleService into it.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -22,11 +22,16 @@ import { ResponseUserDto } from './dtos/user/response-user.dto';
 import { ParseBrPhonePipe } from './pipes/format-br-phone.pipe';
 import { CreateWorkTimeDto } from 'src/work-time/dto/create-work-time.dto';
 import { UpdateWorkTimeDto } from 'src/work-time/dto/update-work-time.dto';
+import { CreateMotorcycleDto } from './dtos/motorcycle/create-motorcycle.dto';
+import { MotorcycleService } from './services/motorcycle.service';
 
 @Controller('user')
 @Roles(Role.Operator, Role.Motoboy, Role.Admin)
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly motorcycleService: MotorcycleService,
+  ) {}
 
   @Get('me')
   async findMe(@Req() req: AuthenticatedRequest) {
@@ -69,11 +74,26 @@ export class UserController {
   @Roles(Role.Admin)
   // @Public()
   async create(
-    @Body() dto: CreateUserDto,
+    @Body() userDto: CreateUserDto,
+    @Body('motorcycle') motorcycleDto: CreateMotorcycleDto,
     @Body('workTime') workTime: CreateWorkTimeDto,
     @Body('phone', ParseBrPhonePipe) phone: string,
   ) {
-    const user = await this.userService.create({ ...dto, phone, workTime });
+    if (userDto.role !== Role.Motoboy) {
+      const user = await this.userService.create({
+        ...userDto,
+        phone,
+        workTime,
+      });
+      return new ResponseUserDto(user);
+    }
+
+    const motorcycle = await this.motorcycleService.create(motorcycleDto);
+    const user = await this.userService.create(
+      { ...userDto, phone, workTime },
+      motorcycle,
+    );
+
     return new ResponseUserDto(user);
   }
 
