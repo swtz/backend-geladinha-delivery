193ab15c34124d0921c07a6586ffd94c3a72ab98
Leonardo Lüders <luderser@hotmail.com>
2026-04-24 11:01:58 -0300

    Added the phone and nickname fields to the LoginDto and set these and the email field as optional using the @IsOptional() decorator from the class-validator package.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,8 +1,24 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 
 export class LoginDto {
+  @IsOptional()
+  @IsPhoneNumber('BR', { message: 'Número de telefone inválido' })
+  phone!: string;
+
+  @IsOptional()
   @IsEmail({}, { message: 'E-mail inválido' })
   email!: string;
+
+  @IsOptional()
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo apelido não pode estar vazio' })
+  nickname!: string;
 
   @IsString({ message: 'Formato inválido' })
   @IsNotEmpty({ message: 'Campo senha não pode estar vazio' })
