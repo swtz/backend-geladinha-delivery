1381beb165f5c1f3bdbbb2f4e1b999ce6b5019ae
Leonardo Lüders <luderser@hotmail.com>
2026-04-27 17:55:30 -0300

    Added a primary key 'id' to the delivery man entity.

--- src/user/entities/delivery-man.entity.ts
+++ src/user/entities/delivery-man.entity.ts
@@ -1,10 +1,20 @@
-import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Motorcycle } from './motorcycle.entity';
 import { Tip } from 'src/tip/entities/tip.entity';
 import { User } from './user.entity';
 
 @Entity()
 export class DeliveryMan {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
   @Column('float')
   daily!: number;
 
