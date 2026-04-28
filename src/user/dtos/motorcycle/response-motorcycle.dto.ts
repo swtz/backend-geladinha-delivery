d44c277a933b67f1d5cb72f93bd1ed89bb40a8fc
Leonardo Lüders <luderser@hotmail.com>
2026-04-28 15:35:01 -0300

    Implemented ResponseMotorcycleDto.

--- src/user/dtos/motorcycle/response-motorcycle.dto.ts
+++ src/user/dtos/motorcycle/response-motorcycle.dto.ts
@@ -1,3 +1,4 @@
+import { Motorcycle } from 'src/user/entities/motorcycle.entity';
 import { UserDtoType } from 'src/user/types/user';
 
 export class ResponseMotorcycleDto {
@@ -10,7 +11,22 @@ export class ResponseMotorcycleDto {
   readonly model: string;
   readonly displacement?: string;
   readonly color: string;
-  readonly isActive: string;
+  readonly isActive: boolean;
   readonly owner: UserDtoType;
   readonly driver: UserDtoType;
+
+  constructor(motorcycle: Motorcycle) {
+    this.id = motorcycle.id;
+    this.createdAt = motorcycle.createdAt;
+    this.updatedAt = motorcycle.updatedAt;
+    this.licensePlate = motorcycle.licensePlate;
+    this.brand = motorcycle.brand;
+    this.year = motorcycle.year;
+    this.model = motorcycle.model;
+    this.displacement = motorcycle.displacement;
+    this.color = motorcycle.color;
+    this.isActive = motorcycle.isActive;
+    this.owner = motorcycle.owner;
+    this.driver = motorcycle.driver.user;
+  }
 }
