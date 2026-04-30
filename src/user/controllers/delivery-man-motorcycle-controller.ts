6ef997bf956bc5fac0a2c739dbb873214037f2b7
Leonardo Lüders <luderser@hotmail.com>
2026-04-30 18:39:13 -0300

    Created DeliveryManMotorcycleController.create().

--- src/user/controllers/delivery-man-motorcycle-controller.ts
+++ src/user/controllers/delivery-man-motorcycle-controller.ts
@@ -1,7 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { DeliveryManMotorcycleService } from '../services/delivery-man-motorcycle.service';
 import { Roles } from 'src/common/role/decorators/roles.decorator';
 import { Role } from 'src/common/role/roles.enum';
+import { CreateUserDto } from '../dtos/user/create-user.dto';
+import { CreateMotorcycleDto } from '../dtos/motorcycle/create-motorcycle.dto';
+import { CreateDeliveryManDto } from '../dtos/delivery-man/create-delivery-man.dto';
 
 @Roles(Role.Admin)
 @Controller('motoboy')
@@ -9,4 +12,19 @@ export class DeliveryManMotorcycleController {
   constructor(
     private readonly deliveryManMotorcycleService: DeliveryManMotorcycleService,
   ) {}
+
+  @Post()
+  async create(
+    @Body('user') userDto: CreateUserDto,
+    @Body('motorcycle') motorcycleDto: CreateMotorcycleDto,
+    @Body('deliveryMan') deliveryManDto: CreateDeliveryManDto,
+  ) {
+    const deliveryMan = await this.deliveryManMotorcycleService.create(
+      userDto,
+      deliveryManDto,
+      motorcycleDto,
+    );
+
+    return deliveryMan;
+  }
 }
