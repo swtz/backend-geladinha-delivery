3fef8bfe8d202d45598a585f0f580e1b76071d5d
Leonardo Lüders <luderser@hotmail.com>
2026-06-01 14:33:53 -0300

    Optimized validateFindOneParamsOrFail<T>().

--- src/common/utils/validate-find-one-params-or-fail.ts
+++ src/common/utils/validate-find-one-params-or-fail.ts
@@ -4,22 +4,23 @@ export function validateFindOneParamsOrFail<T extends Record<string, unknown>>(
   dto: Partial<T>,
   idFromDto?: boolean,
 ): Partial<T> {
-  const hasId = Object.hasOwn(dto, 'id');
-  const hasValidId = !!dto['id'];
+  const error = new BadRequestException('Informe os dados para consulta');
   const hasDefinedParam = Object.values(dto).some(value => {
     return value != null;
   });
-  const error = new BadRequestException('Informe os dados para consulta');
 
-  if (idFromDto) {
-    if (!hasId || !hasValidId) {
+  if (!idFromDto) {
+    if (!hasDefinedParam) {
       throw error;
     }
 
     return dto;
   }
 
-  if (!hasDefinedParam) {
+  const hasId = Object.hasOwn(dto, 'id');
+  const hasValidId = !!dto['id'];
+
+  if (!hasId || !hasValidId) {
     throw error;
   }
 
