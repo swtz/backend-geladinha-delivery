49f2921db4a2724dedbc48a0d2ddbba1cc8c5722
Leonardo Lüders <luderser@hotmail.com>
2026-06-01 15:35:06 -0300

    Updated UpdateCustomerDto.

--- src/customer/dto/update-customer.dto.ts
+++ src/customer/dto/update-customer.dto.ts
@@ -1,12 +1,4 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateCustomerDto } from './create-customer.dto';
-import { UpdateAddressDto } from 'src/address/dto/update-address.dto';
-import { IsNotEmptyObject, IsOptional } from 'class-validator';
 
-export class UpdateCustomerDto extends PartialType(
-  OmitType(CreateCustomerDto, ['address']),
-) {
-  @IsOptional()
-  @IsNotEmptyObject({}, { message: 'Formato inválido' })
-  address?: UpdateAddressDto;
-}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
