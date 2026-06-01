c98ab36117834d0af7c88651fce979c9e8213d59
Leonardo Lüders <luderser@hotmail.com>
2026-06-01 15:33:45 -0300

    Updated CreateCustomerDto adding the entity's new fields.

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,19 +1,28 @@
 import {
   IsNotEmpty,
-  IsNotEmptyObject,
+  IsOptional,
   IsPhoneNumber,
   IsString,
 } from 'class-validator';
-import { CreateAddressDto } from 'src/address/dto/create-address.dto';
 
 export class CreateCustomerDto {
   @IsString({ message: 'Formato inválido' })
   @IsNotEmpty({ message: 'Campo nome não pode estar vazio' })
   name!: string;
 
+  @IsOptional()
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo sobrenome não pode estar vazio' })
+  lastName!: string;
+
+  @IsString({ message: 'Formato inválido' })
+  @IsNotEmpty({ message: 'Campo apelido não pode estar vazio' })
+  nickname!: string;
+
   @IsPhoneNumber('BR', { message: 'Número de telefone inválido' })
   phone!: string;
 
-  @IsNotEmptyObject({}, { message: 'Formato inválido' })
-  address!: CreateAddressDto;
+  @IsOptional()
+  @IsPhoneNumber('BR', { message: 'Número de telefone inválido' })
+  secondPhone!: string;
 }
