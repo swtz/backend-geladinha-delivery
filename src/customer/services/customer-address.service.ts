7892e28bd58b66c9dc4431a231861d7095c688c5
Leonardo Lüders <luderser@hotmail.com>
2026-06-06 10:22:13 -0300

    feat: create a util method to format a gived string

--- src/customer/services/customer-address.service.ts
+++ src/customer/services/customer-address.service.ts
@@ -2,6 +2,7 @@ import { AddressService } from 'src/address/address.service';
 import { CustomerService } from '../customer.service';
 import { CreateCustomerDto } from '../dto/create-customer.dto';
 import { CreateAddressDto } from 'src/address/dto/create-address.dto';
+import { transformToLowerCase } from 'src/common/utils/transform-to-lower-case';
 
 export class CustomerAddressService {
   constructor(
@@ -10,6 +11,14 @@ export class CustomerAddressService {
   ) {}
 
   async create(customerDto: CreateCustomerDto, addressDto: CreateAddressDto) {
+    const fields = Object.keys(customerDto).filter(
+      field => typeof customerDto[field] === 'string',
+    );
+
+    for (const field of fields) {
+      transformToLowerCase(customerDto[field] as string);
+    }
+
     const customer = await this.customerService.create(customerDto);
     const address = await this.addressService.create(addressDto);
     const created = await this.customerService.save({
