8712cafad8b4e98d37e50c26eece64d2c2fc753d
Leonardo Lüders <luderser@hotmail.com>
2026-08-12 12:31:49 -0300

    refactor: update validateUniqueFields 'validator' constant type using 'Partial<Place>' to allow UpdatePlaceDto argument type

--- src/place/services/place-fields-validation.service.ts
+++ src/place/services/place-fields-validation.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { CreatePlaceDto } from '../dto/create-place.dto';
 import { CreatePlaceDtoForValidator } from '../types/place-validator.type';
 import { PlaceService } from '../services/place.service';
+import { UpdatePlaceDto } from '../dto/update-place.dto';
+import { Place } from '../entities/place.entity';
 
 @Injectable()
 export class PlaceFieldsValidationService {
   constructor(private readonly placeService: PlaceService) {}
 
-  async validateUniqueFields(dto: CreatePlaceDto) {
-    const validator: CreatePlaceDtoForValidator = {
+  async validateUniqueFields(dto: CreatePlaceDto | UpdatePlaceDto) {
+    const validator: Partial<Place> = {
       name: dto.name,
       businessName: dto.businessName,
       code: dto.code,
