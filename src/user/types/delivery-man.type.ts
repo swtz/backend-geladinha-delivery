c5c9e6f13b830ddfcd3e8ba85b116a571b94b36d
Leonardo Lüders <luderser@hotmail.com>
2026-08-12 17:12:42 -0300

    update: update FindDeliveryManByUserDataType

--- src/user/types/delivery-man.type.ts
+++ src/user/types/delivery-man.type.ts
@@ -1,4 +1,5 @@
 import { DeliveryMan } from '../entities/delivery-man.entity';
+import { Motorcycle } from '../entities/motorcycle.entity';
 import { User } from '../entities/user.entity';
 
 export type DeliveryManType = Omit<
@@ -8,7 +9,8 @@ export type DeliveryManType = Omit<
 
 export type FindDeliveryManByUserDataType = Omit<
   Partial<DeliveryMan>,
-  'user'
+  'user' | 'motorcycle'
 > & {
-  user: Partial<User>;
+  user?: Partial<User>;
+  motorcycle?: Partial<Motorcycle>;
 };
